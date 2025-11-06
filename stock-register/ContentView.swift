81309079ccb2747c1672f8c9b3488e137fcb35bd
Rajat Mittal <rajatmittal2012@gmail.com>
2025-11-07 01:36:02 +0530

    Initial Commit

---
+++ stock-register/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  stock-register
+//
+//  Created by Rajat Mittal on 07/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ stock-register/stock_registerApp.swift
@@ -0,0 +1,17 @@
+//
+//  stock_registerApp.swift
+//  stock-register
+//
+//  Created by Rajat Mittal on 07/11/25.
+//
+
+import SwiftUI
+
+@main
+struct stock_registerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
